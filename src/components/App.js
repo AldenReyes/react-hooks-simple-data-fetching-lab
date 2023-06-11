9f3c91a22c55b380f3b6d6518a16e746beb2d06c
AldenReyes <122179486+AldenReyes@users.noreply.github.com>
2023-06-11 03:40:36 -0700

    Completes Lab: Adds App.js which renders image tag from fetched data

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,19 @@
-// create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [dogRandom, setDogRandom] = useState("");
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => setDogRandom(data.message));
+  }, []);
+
+  if (!dogRandom) {
+    return <p>Loading...</p>;
+  } else {
+    return <img src={dogRandom} alt="A Random Dog"></img>;
+  }
+}
+
+export default App;
